0a8a07a0610bc63825c46a377e41de906348e74a
Ulann-Afk24 <152866195+Ulann-Afk24@users.noreply.github.com>
2024-02-24 01:06:44 +0800

    initial D 2

--- app/src/main/java/com/example/navigations/HomeFragment.kt
+++ app/src/main/java/com/example/navigations/HomeFragment.kt
@@ -1,21 +1,35 @@
 package com.example.navigations
 
 import android.os.Bundle
+import android.provider.ContactsContract.Profile
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, true)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+
+        val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnClickListener{
+            when(it.itemId){
+
+            }
+        }
+
+
+        return view
+
     }
+
+
 }
--- app/src/main/java/com/example/navigations/MainActivity.kt
+++ app/src/main/java/com/example/navigations/MainActivity.kt
@@ -3,44 +3,96 @@ package com.example.navigations
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.example.navigations.bottom.Acsesorinav_fragment
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener{
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true){
+        override fun handleOnBackPressed() {
+            onBackPressedMethod()
+        }
 
 
-    private lateinit var fragmentManager:FragmentManager
+    }
+
+    private fun onBackPressedMethod() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            (drawerLayout.closeDrawer(GravityCompat.START))
 
 
+
+        }else{
+            finish()
+        }
+    }
+
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        onBackPressedDispatcher.addCallback(this,onBackPressedCallback)
 
 
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        val navigationView = findViewById<NavigationView>(R.id.navigation_drawer)
+        val header = navigationView.getHeaderView(0)
+
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+        ///deaf nav pri
+        replaceframent(HomeFragment())
+        navigationView.setCheckedItem(R.id.nav_helmet)
 
     }
-    
+    private fun replaceframent(fragment:HomeFragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.nav_fragment,fragment)
+            .commit()
+
+    }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.nav_prime -> openfragment(PrimeFragment())
-            R.id.nav_motor -> openfragment(MotorFragment())
-            R.id.bottom_home -> openfragment((HomeFragment()))
-            R.id.bottom_profile -> openfragment((ProfileFragment()))
-            R.id.bottom_cart -> openfragment(CartFragment())
-            R.id.bottom_order -> openfragment(Ordersragment())
+            R.id.nav_helmet -> {
+                replaceframent(HomeFragment())
+                title = "helmet_sa_nav_menu"
+            }
+            R.id.nav_motor-> {
+                replaceframent(HomeFragment())
+                title = "motor_sa_nav_menu"
+                Toast.makeText(this,"share Clicked", Toast.LENGTH_LONG).show()
+            }
         }
-        return true
-    }
-    private fun openfragment(fragment: Fragment) {
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container, fragment)
-        fragmentTransaction.commit()
+        drawerLayout.closeDrawer(GravityCompat.START)
 
+        return TODO("Provide the return value")
     }
 
 
